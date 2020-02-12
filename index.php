08a2942e8ed83bb691bf809e628b82f1c2803d1e
rralph091 <60122569+rralph091@users.noreply.github.com>
2020-02-12 22:32:23 +0800

    Update index.php

--- index.php
+++ index.php
@@ -40,7 +40,7 @@ $navigation = (isset($_GET['navigation']) && $_GET['navigation'] != '') ? $_GET[
                         ?>
                         </div>
 
-                        <div id="footer">
+                  <div style="background-color:lightblue"> <div id="footer"> </div>
                         <div class="index"><div style="background-color:lightblue"> <div class="font-title"> RELAX AND LIVE A LITTLE! </div></div></div>
                         </div>
     </div>
