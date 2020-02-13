49cba2d44bf12e87f48eebccd72491b404f53313
rralph091 <60122569+rralph091@users.noreply.github.com>
2020-02-13 09:50:19 +0800

    Update home.php

--- home.php
+++ home.php
@@ -1,11 +1,22 @@
 <html>
+   <link rel="stylesheet" type="text/css" href="style.css">
+   <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
    <body>
 <div class="card">
-      <h2>LEAGUE OF LEGENDS</h2>
-      <h5>Title description, Sep 2, 2017</h5>
-      <div class="fakeimg" style="height:200px;">Image</div>
-      <p>Some text..</p>
-      <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+      <h2>ENTERTAINMENT HISTORY</h2>
+      <p>The "ancient craft of communicating events and experiences, using words, images, sounds and gestures" by telling a story 
+         is not only the means by which people passed on their cultural values and traditions and history from one generation to another, 
+         it has been an important part of most forms of entertainment ever since the earliest times. Stories are still told in the early forms, 
+         for example, around a fire while camping, or when listening to the stories of another culture as a tourist. 
+         "The earliest storytelling sequences we possess, now of course, committed to writing, were undoubtedly originally a speaking from mouth to ear and their force as entertainment derived from the very same elements we today enjoy in films and novels.
+         "Storytelling is an activity that has evolved and developed "toward variety". Many entertainments, including storytelling but especially music and drama, remain familiar but have developed into a wide variety of form to suit a very wide range of personal preferences and cultural expression. 
+         Many types are blended or supported by other forms. For example, drama, stories and banqueting (or dining) are commonly enhanced by music; sport and games are incorporated into other activities to increase appeal. 
+         Some may have evolved from serious or necessary activities (such as running and jumping) into competition and then become entertainment. 
+         It is said, for example, that pole vaulting "may have originated in the Netherlands, where people used long poles to vault over wide canals rather than wear out their clogs walking miles to the nearest bridge. 
+         Others maintain that pole vaulting was used in warfare to vault over fortress walls during battle."The equipment for such sports has become increasingly sophisticated. 
+         Vaulting poles, for example, were originally made from woods such as ash, hickory or hazel; in the 19th century bamboo was used and in the 21st century poles can be made of carbon fibre. 
+         Other activities, such as walking on stilts, are still seen in circus performances in the 21st century. Gladiatorial combats, also known as "gladiatorial games", popular during Roman times, 
+         provide a good example of an activity that is a combination of sport, punishment, and entertainment.</p>
     </div>
     </body>
 </html>
